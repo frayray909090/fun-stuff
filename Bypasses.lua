247be7e254a9b0ec0b81679266ca79acea8a8d4e
Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>
2022-10-30 19:15:21 -0400

    Add files via upload

---
+++ Bypasses.lua
@@ -0,0 +1,91 @@
+return {
+    -- COMBINATIONS
+    ["dickhead"] = "di{{vzxzczxczvz:ck}}he{{AIeIXzVZx:ad}}",
+    ["fucking"] = "fu{{AIeIXzVZx:ck}}in{{AIeIXzVZx:g}}",
+    ["motherfucking"] = "motherf{{zczxczvzaiei:uc}}kin{{AIeIXzVZx:g}}",
+    -- NORMALS
+    ["ass"] = "as{{AIeIXzVZx:s}}",
+    ["beaner"] = "beane{{AIeIXzVZx:r}}", -- Unstable
+    ["bitch"] = "bit{{AIeIXzVZx:ch}}",
+    ["blowjob"] = "blow{{AIeIXzVZx:job}}",
+    ["boob"] = "boo{{AIeIXzVZx:b}}",
+    ["boner"] = "bon{{AIeIXzVZx:er}}", -- Unstable
+    ["cock"] = "co{{AIeIXzVZx:ck}}",
+    ["cunt"] = "cu{{AIeIXzVZx:nt}}",
+    ["cum"] = "cu{{AIeIXzVZx:m}}",
+    ["dick"] = "di{{zczxczvz:ck}}",
+    ["dildo"] = "di{{AIeIXzVZx:ldo}}",
+    ["dumb"] = "du{{AIeIXzVZx:mb}}", -- Unstable
+    ["dumbass"] = "dumba{{AIeIXzVZx:ss}}", -- Unstable
+    ["dyke"] = "dy{{AIeIXzVZx:ke}}",
+    ["fag"] = "fa{{AIeIXzVZx:g}}",
+    ["fuck"] = "fu{{AIeIXzVZx:ck}}",
+    ["gaylord"] = "gayl{{AIeIXzVZx:ord}}",
+    ["hoe"] = "h{{zczxczvz:oe}}",
+    ["jew"] = "je{{zczxczvz:w}}",
+    ["jizz"] = "ji{{AIeIXzVZx:zz}}",
+    ["kike"] = "ki{{AIeIXzVZx:ke}}",
+    ["kkk"] = "k{{AIeIXzVZx:kk}}",
+    ["kyke"] = "ky{{AIeIXzVZx:ke}}",
+    ["masturbat"] = "mastur{{AIeIXzVZx:bat}}",
+    ["motherfucker"] = "motherf{{zczxczvzaiei:ucker}}",
+    ["nazi"] = "na{{AIeIXzVZx:zi}}",
+    ["nigga"] = "ni{{AIeIXzVZx:gga}}",
+    ["nigger"] = "ni{{AIeIXzVZx:gger}}",
+    ["piss"] = "pi{{zczxczvz:ss}}",
+    ["penis"] = "pe{{AIeIXzVZx:nis}}",
+    ["pussy"] = "pu{{AIeIXzVZx:ssy}}",
+    ["rap"] = "ra{{AIeIXzVZx:p}}",  -- Unstable
+    ["retard"] = "reta{{AIeIXzVZx:rd}}",
+    ["sex"] = "se{{AIeIXzVZx:x}}",
+    ["shit"] = "sh{{AIeIXzVZx:it}}",
+    ["slut"] = "sl{{zczxczvz:ut}}",
+    ["sperm"] = "spe{{AIeIXzVZx:rm}}",
+    ["stfu"] = "st{{AIeIXzVZx:fu}}",
+    ["stupid"] = "stu{{AIeIXzVZx:pid}}",
+    ["testicle"] = "test{{AIeIXzVZx:icle}}",
+    ["tits"] = "ti{{AIeIXzVZx:ts}}",
+    ["vagina"] = "va{{AIeIXzVZx:gi}}na", -- Unstable
+    ["whore"] = "who{{AIeIXzVZx:re}}",
+    ["rape"] = "ra{{AIeIXzVZx:pe}}",
+    ["molest"] = "mol{{AIeIXzVZx:est}}",
+    -- MISC.
+    ["discord"] = "disco{{zczxczvz:rd}}",
+    ["kys"] = "k{{AIeIXzVZx:ys}}",
+    ["kill"] = "ki{{zczxczvz:ll}}",
+    ["wtf"] = "wt{{AIeIXzVZx:f}}",
+    ["the"] = "th{{AIeIXzVZx:e}}",
+    ["tho"] = "th{{AIeIXzVZx:o}}", -- Unstable
+    ["lmao"] = "l{{AIeIXzVZx:mao}}",
+    ["lmfao"] = "l{{AIeIXzVZx:mfao}}",
+    ["suck"] = "suc{{AIeIXzVZx:k}}",
+    ["nuts"] = "n{{AIeIXzVZx:uts}}", -- Unstable
+    ["you"] = "y{{AIeIXzVZx:ou}}"
+    ["milf"] = "mi{{AIeIXzVZx:elf}}",
+    ["bitch boy"] = "bit{{AIeIXzVZx:ch}} b{{AIeIXzVZx:oy}}",
+    ["idfc"] = "id{{AIeIXzVZx:fc}}",
+    ["idfk"] = "id{{AIeIXzVZx:fk}}",
+    -- Websites.
+    ["pornhub"] = "porn{{zczxczvz:hub}}",
+    ["onlyfans"] = "onlyfa{{AIeIXzVZx:ns}}",
+    ["spotify"] = "spoti{{zczxczvz:fy}}",
+    ["xxxvids"] = "xxxvi{{AIeIXzVZx:ds}}",
+    --spanish
+    ["perra"] = "per{{zczxczvz:ra}}",
+    ["chico"] = "chi{{zczxczvz:co}}",
+    ["chico perra"] = "chico per{{zczxczvz:ra}}",
+
+    --french
+    ["Connard"] = "conna{{zczxczvz:rd}}",
+    ["vagin"] = "vag{{zczxczvz:in}}",
+    ["vagina"] = "vagi{{zczxczvz:na}}",
+    ["chatte"] = "chat{{zczxczvz:te}}",
+    ["bite"] = "bi{{zczxczvz:te}}",
+    ["gueule"] = "gueu{{zczxczvz:le}}",
+    ["hitler"] = "hitl{{zczxczvz:er}}",
+    ["juif"] = "ju{{zczxczvz:if}}",
+    ["basterd"] = "baste{{zczxczvz:rd}}",
+    ["Ferme ta gueule"] = "fer{{zczxczvz:me}}t{{zczxczvz:a}}gueu{{zczxczvz:le}}",
+    ["Trou du cul"] = "tr{{zczxczvz:ou}}d{{zczxczvz:u}}cu{{zczxczvz:l}}",
+    ["cul"] = "cu{{zczxczvz:l}}",
+}
---
+++ ChatBypass.lua
@@ -0,0 +1,37 @@
+local Bypasses = loadstring(game:HttpGet(../Bypasses.lua))()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remote = ReplicatedStorage:FindFirstChild("SayMessageRequest", true)
+
+local ChatBypass; ChatBypass = hookmetamethod(Remote, "__namecall", function(self, ...)
+    local Method = getnamecallmethod()
+    local Arguments = {...}
+    
+    if self == Remote and Method == "FireServer" then
+        local Message = Arguments[1]
+        local Split = Message:split(" ")
+        local FinalMessage = ""
+
+        for _, x in next, Split do
+            for _, Bypass in next, Bypasses do
+                if x:lower() == _ then
+                    if x:upper() ~= x then
+                        Message = Message:gsub(x, Bypass)
+                        FinalMessage = Message .. " fn"
+                    else
+                        Message = Message:gsub(x, Bypass):upper()
+                        FinalMessage = Message:gsub(x, Bypass):upper() .. " fn"
+                    end
+                end
+            end
+        end
+        
+        if FinalMessage ~= "" then
+            Arguments[1] = FinalMessage
+        end
+        
+        return ChatBypass(self, unpack(Arguments))
+    end
+    
+    return ChatBypass(self, ...)
+end)
